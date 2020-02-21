fe5ed437fd4f9005d8c6bc5b999c50b853456b54
summer <lv.summer@qq.com>
2020-02-21 14:03:12 +0800

    增加一系列用户注册方法

--- src/main/java/com/shuwa/treefrog/service/IUserService.java
+++ src/main/java/com/shuwa/treefrog/service/IUserService.java
@@ -2,6 +2,8 @@ package com.shuwa.treefrog.service;
 
 import com.shuwa.treefrog.entity.User;
 
+import java.util.List;
+
 public interface IUserService {
     /**
      * 用户登录
@@ -24,5 +26,50 @@ public interface IUserService {
      * @return
      */
     User get(Integer id);
+
+    /**
+     * 用户注册
+     *
+     * @return
+     */
+    boolean register(User user);
+
+    /**
+     * 将传递过来的用户名、手机号码检查是否在数据库中有相同值
+     *
+     * @param userName
+     * @param phone
+     * @return
+     */
+    boolean isAllowRegister(String username, String phone);
+
+    /**
+     * 检查手机号是否已注册
+     * @param phone 手机号
+     * @return
+     */
+    boolean isPhoneExist(String phone);
+
+
+    /**
+     * 检查用户名是否存在
+     * @param username
+     * @return
+     */
+    boolean isUserNameExist(String username);
+
+    /**
+     * 用户删除-管理员功能
+     *
+     * @param id
+     * @return
+     */
+    boolean deleteUser(int id);
+
+    /**
+     * 得到全部用户
+     * @return
+     */
+    List<User> getAllUser();
 
 }
