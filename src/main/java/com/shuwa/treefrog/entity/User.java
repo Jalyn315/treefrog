30ae43c18617ab6b7a61fc9009e2e0edad02fff3
summer <lv.summer@qq.com>
2020-02-21 20:10:39 +0800

    无改动，仅提交

--- src/main/java/com/shuwa/treefrog/entity/User.java
+++ src/main/java/com/shuwa/treefrog/entity/User.java
@@ -1,7 +1,10 @@
 package com.shuwa.treefrog.entity;
 
+import org.springframework.stereotype.Component;
+
 import java.io.Serializable;
 
+@Component
 public class User implements Serializable {
     private Integer id;
     private String username;
