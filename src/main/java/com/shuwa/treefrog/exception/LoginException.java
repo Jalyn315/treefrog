4ccd1815fd3d183ed586b073d9f2224472e8a9e2
summer <lv.summer@qq.com>
2020-02-26 15:30:23 +0800

    新增登录异常

---
+++ src/main/java/com/shuwa/treefrog/exception/LoginException.java
@@ -0,0 +1,16 @@
+package com.shuwa.treefrog.exception;
+
+/**
+ * 自定义 登录 异常
+ */
+public class LoginException extends Exception {
+    //无参构造方法
+    public LoginException(){
+        super();
+    }
+
+    //有参的构造方法
+    public LoginException(String message){
+        super(message);
+    }
+}
