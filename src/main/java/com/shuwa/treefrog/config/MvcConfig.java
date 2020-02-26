99a66f28cc196f3ebd66ad25adb0f94b38d329c9
summer <lv.summer@qq.com>
2020-02-26 15:29:38 +0800

    仅提交，无修改

--- src/main/java/com/shuwa/treefrog/config/MvcConfig.java
+++ src/main/java/com/shuwa/treefrog/config/MvcConfig.java
@@ -1,10 +1,8 @@
 package com.shuwa.treefrog.config;
 
 
-import com.shuwa.treefrog.interceptor.UserInterceptor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
