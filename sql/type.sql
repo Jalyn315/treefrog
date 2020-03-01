e5780a7aa2035a4f6f52ae03f1e53659e6952bc6
summer <lv.summer@qq.com>
2020-03-01 18:18:48 +0800

    测试所使用的 type.sql 文件

--- sql/type.sql
+++ sql/type.sql
@@ -1,6 +1,6 @@
 /*
-SQLyog Ultimate v12.09 (64 bit)
-MySQL - 5.7.28 : Database - treefrog
+SQLyog Ultimate v10.00 Beta1
+MySQL - 8.0.12 : Database - treefrog
 *********************************************************************
 */
 
@@ -14,19 +14,20 @@ MySQL - 5.7.28 : Database - treefrog
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`treefrog` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `treefrog`;
-
 /*Table structure for table `type` */
 
 DROP TABLE IF EXISTS `type`;
 
 CREATE TABLE `type` (
   `id` int(5) NOT NULL AUTO_INCREMENT COMMENT '分类ID',
-  `name` varchar(20) COLLATE utf8_bin DEFAULT NULL COMMENT '分类名称',
+  `type_name` varchar(20) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '分类名称',
+  `create_time` date DEFAULT NULL COMMENT '创建分类时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 /*Data for the table `type` */
+
+insert  into `type`(`id`,`type_name`,`create_time`) values (2,'文件','2020-03-01'),(3,'视频','2020-03-02'),(4,'文档','2020-03-03'),(5,'学习资料','2020-03-01'),(6,'学习视频','2020-03-01');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
