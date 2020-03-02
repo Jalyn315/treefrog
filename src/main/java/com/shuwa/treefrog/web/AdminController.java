a7467e3837e139cf2f99275fc96330324a3a931c
tom_glb <goodMorning_pro@Jalyn.com>
2020-03-02 21:50:06 +0800

    1.修复bug

--- src/main/java/com/shuwa/treefrog/web/AdminController.java
+++ src/main/java/com/shuwa/treefrog/web/AdminController.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
@@ -177,15 +178,15 @@ public class AdminController {
      * @return
      */
     @PostMapping(value = "/saveType")
-    public String saveType(@RequestParam("typeName") String typeName, Model model) {
+    public String saveType(@RequestParam("typeName") String typeName, RedirectAttributes model) {
         logger.info("AdminController->saveType");
         try {
             typeService.addType(typeName, new Date());
         } catch (TypeNameException e) {
-            model.addAttribute("addTypeError", e.getMessage());
+            model.addFlashAttribute("addTypeError", e.getMessage());
         }
-        model.addAttribute("page", new PageParam());
-        return "redirect:/types/1";
+        model.addFlashAttribute("page", new PageParam());
+        return "redirect:/admin/types/1";
     }
 
     /**
