dec36015f7bd413f0a6076efa9b07bb6aeb9d194
tom_glb <1921776332@qq.com>
2020-03-03 17:35:42 +0800

    新增download.sql文件

---
+++ sql/download.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.28 : Database - treefrog
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`treefrog` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `treefrog`;
+
+/*Table structure for table `download` */
+
+DROP TABLE IF EXISTS `download`;
+
+CREATE TABLE `download` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `user_name` varchar(18) COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '用户',
+  `file_name` varchar(250) COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '文件名称',
+  `file_url` varchar(1024) COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '本地路径',
+  `file_size` bigint(20) NOT NULL DEFAULT '0' COMMENT '文件大小',
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '下载时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `download` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
