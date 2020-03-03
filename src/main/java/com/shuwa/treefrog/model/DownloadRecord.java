d015abac64557eb3141a0efb55c139b589ded769
tom_glb <1921776332@qq.com>
2020-03-03 17:36:09 +0800

    1.新增下载记录类

---
+++ src/main/java/com/shuwa/treefrog/model/DownloadRecord.java
@@ -0,0 +1,61 @@
+package com.shuwa.treefrog.model;
+
+import java.util.Date;
+
+public class DownloadRecord {
+
+    private long id;
+    private String userName;
+    private String fileName;
+    private String fileUrl;
+    private String fileSize;
+    private  Date downloadTime;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileUrl() {
+        return fileUrl;
+    }
+
+    public void setFileUrl(String fileUrl) {
+        this.fileUrl = fileUrl;
+    }
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public Date getDownloadTime() {
+        return downloadTime;
+    }
+
+    public void setDownloadTime(Date downloadTime) {
+        this.downloadTime = downloadTime;
+    }
+}
