063bc59f7a069ec2efa438b59866b05ab96e8b82
tom_glb <1921776332@qq.com>
2020-03-06 16:31:47 +0800

    1.完善重置密码功能的Ajax请求

--- src/main/resources/static/adminStyle/asset/js/userlist.js
+++ src/main/resources/static/adminStyle/asset/js/userlist.js
@@ -29,11 +29,6 @@ function searchList(strValue) {
         });
     }
 }
-
-
-
-
-
 function  checkbox() {
     //change表示当当状态改变时，可用来判断是否被选中
     $('tbody input').change(function () {
@@ -80,17 +75,25 @@ function resetPassword() {
     $('.delete').click(function () {
         var aUserName = [];
         var index = 0;
+        var aId = [];
         var user_index = 0;  //username数组的下标
-        $('#dispose-box').show();
         $('#dispose-box .selectbox').hide();
         $('#dispose-box .userp').show();
         $('.titlebox h4').html('重置用户密码');
         $('.permissionBuddle .message p').html('新密码')
         $('tbody input').each(function () {
             if($(this).prop('checked')){
-             aUserName[index++]= $(this).parent().siblings().eq(1).text();
+             aUserName[index]= $(this).parent().siblings().eq(1).text();
+             aId[index] = $(this).parent().siblings().eq(0).text(); //获取id
+                index++;
             }
         });
+        if (index == 0){
+            $('.msgbox #info').html('至少选择一个!');
+            $('.msgbox').show();
+        }else {
+            $('#dispose-box').show();  //如果一个都不选择则无法打开
+        }
         $('#dispose-box .usern  input').val(aUserName[user_index]); //姓名框显示的第一个姓名
         $('#last1').click(function () {
                 if(user_index != 0){
@@ -102,6 +105,26 @@ function resetPassword() {
                 $('#dispose-box .usern  input').val(aUserName[++user_index]);
             }
         });
-
+        $('#update').click(function () {
+            $.post({
+                url:"/resetPassword",
+                data:{"password":$('#newPassword').val(),"id":aId[user_index]},
+                success:function (data) {
+                    $('.msgbox #info').html(data);
+                    $('.msgbox').show();
+                }
+            });
+        });
+    });
+}
+function msg() {
+    var html = "<div class=\"msgbox\">\n" +
+        "            <div class=\"msgtitle\"><h4>提示</h4></div>\n" +
+        "            <p id=\"info\">修改成功</p>\n" +
+        "            <button id=\"sure\">确定</button>\n" +
+        "        </div>";
+    $('body').append(html);
+    $('#sure').click(function () {
+        $('.msgbox').hide();
     });
 }
